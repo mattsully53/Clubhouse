d74fc585ee1ee453186e6ba9546f7423485d74b9
Zachary Willis <zwillis@arriv.io>
2017-10-12 19:51:51 -0500

    Adds a new view when a use logs in

--- ClubHouseApp/app/src/main/java/com/softwareengineering/clubhouseapp/LoginActivity.java
+++ ClubHouseApp/app/src/main/java/com/softwareengineering/clubhouseapp/LoginActivity.java
@@ -3,6 +3,7 @@ package com.softwareengineering.clubhouseapp;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -28,6 +29,7 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -314,17 +316,23 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             } catch (InterruptedException e) {
                 return false;
             }
-
+            /* Here is where we should ask the API for the user with the given email and password.
+             * If one is returned, we let them in.
+             * If not, the user doesn't exist.
+             */
             for (String credential : DUMMY_CREDENTIALS) {
                 String[] pieces = credential.split(":");
                 if (pieces[0].equals(mEmail)) {
                     // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
+                    if (pieces[1].equals(mPassword)){
+                        Intent intent = new Intent(LoginActivity.this, UserProfileActivity.class);
+                        intent.putExtra("user", pieces[0]);
+                        startActivity(intent);
+                        return true;
+                    }
                 }
             }
-
-            // TODO: register the new account here.
-            return true;
+            return false;
         }
 
         @Override
@@ -335,8 +343,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             if (success) {
                 finish();
             } else {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
+                Toast.makeText(LoginActivity.this, "The provided username or password were incorrect.", Toast.LENGTH_LONG).show();
             }
         }
 
---
+++ ClubHouseApp/app/src/main/java/com/softwareengineering/clubhouseapp/UserProfileActivity.java
@@ -0,0 +1,23 @@
+package com.softwareengineering.clubhouseapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("user");
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
