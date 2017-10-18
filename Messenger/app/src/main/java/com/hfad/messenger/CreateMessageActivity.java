187f800c9ed7b3a5108cf0ca85882449cc15e39e
Matthew Sullivan <msullivan@troy.edu>
2017-10-17 21:10:31 -0500

    ClubHouse changes?

---
+++ Messenger/app/src/main/java/com/hfad/messenger/CreateMessageActivity.java
@@ -0,0 +1,18 @@
+package com.hfad.messenger;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class createMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    public void onSendMessage (View view) {
+
+    }
+}
