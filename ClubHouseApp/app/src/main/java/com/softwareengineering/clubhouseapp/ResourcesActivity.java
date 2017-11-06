d39a96cb2f5a2d9b8363d98e0cd33304d8cbf4b2
Rachel Carter <rcarter149631@troy.edu>
2017-11-06 14:18:45 -0600

    RLC

--- ClubHouseApp/app/src/main/java/com/softwareengineering/clubhouseapp/ResourcesActivity.java
+++ ClubHouseApp/app/src/main/java/com/softwareengineering/clubhouseapp/ResourcesActivity.java
@@ -1,11 +1,21 @@
 package com.softwareengineering.clubhouseapp;
 
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by RachelLynCarter on 11/6/17.
- */
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.content.Intent;
 
 public class ResourcesActivity extends AppCompatActivity {
 
-}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resources);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+    }
+
