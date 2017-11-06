3ea395ee576f0b5ebb62721c079af1b43b3b7cfb
Rachel Carter <rcarter149631@troy.edu>
2017-11-06 13:43:17 -0600

    Rachel (ResourceActivity) {still working}

---
+++ ClubHouseApp/app/src/main/java/com/softwareengineering/clubhouseapp/ResourcesActivity.java
@@ -0,0 +1,11 @@
+package com.softwareengineering.clubhouseapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by RachelLynCarter on 11/6/17.
+ */
+
+public class ResourcesActivity extends AppCompatActivity {
+
+}
