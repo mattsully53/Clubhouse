ad6f170063fcc62740d45f47612ead97795740e4
Zachary Willis <zwillis@arriv.io>
2017-11-29 20:54:28 -0600

    Adds logic to ensure usable emails and passwords

--- ClubHouseApp/app/src/main/java/com/softwareengineering/clubhouseapp/CreateAccountActivity.java
+++ ClubHouseApp/app/src/main/java/com/softwareengineering/clubhouseapp/CreateAccountActivity.java
@@ -7,6 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -30,25 +31,32 @@ public class CreateAccountActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+
                 mNameView = (EditText) findViewById(R.id.name);
                 mEmailView = (EditText) findViewById(R.id.email);
                 mPasswordView = (EditText) findViewById(R.id.password);
                 mBioView = (EditText) findViewById(R.id.bio);
 
-                boolean check;
+                String email = mEmailView.getText().toString();
+                String password = mPasswordView.getText().toString();
 
-                SQLiteOpenHelper clubhouseDatabaseHelper = new ClubhouseDatabaseHelper(CreateAccountActivity.this);
-                SQLiteDatabase db = clubhouseDatabaseHelper.getWritableDatabase();
-                check = ClubhouseDatabaseHelper.insertUser(db, mNameView.getText().toString(), mEmailView.getText().toString(),
-                        mPasswordView.getText().toString(),
-                        mBioView.getText().toString(),
-                        R.drawable.blank_profile);
+                if(validateEmailAndPassword(email, password)) {
 
-                if (check){
-                    Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
-                    startActivity(intent);
-                } else {
-                    Toast.makeText(CreateAccountActivity.this, "Something went wrong.", Toast.LENGTH_LONG).show();
+                    boolean check;
+
+                    SQLiteOpenHelper clubhouseDatabaseHelper = new ClubhouseDatabaseHelper(CreateAccountActivity.this);
+                    SQLiteDatabase db = clubhouseDatabaseHelper.getWritableDatabase();
+                    check = ClubhouseDatabaseHelper.insertUser(db, mNameView.getText().toString(), mEmailView.getText().toString(),
+                            mPasswordView.getText().toString(),
+                            mBioView.getText().toString(),
+                            R.drawable.blank_profile);
+
+                    if (check) {
+                        Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(CreateAccountActivity.this, "Something went wrong.", Toast.LENGTH_LONG).show();
+                    }
                 }
             }
         });
@@ -56,6 +64,21 @@ public class CreateAccountActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         if(actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(false);
+        }
+    }
+
+    private boolean validateEmailAndPassword(String email, String password){
+        // Check for a valid password, if the user entered one.
+        if ((TextUtils.isEmpty(password) || password.length() < 5)) {
+            Toast.makeText(CreateAccountActivity.this, "The password must have more than 4 characters.", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        // Check for a valid email address.
+        if (email.contains("@")) {
+            return true;
+        } else {
+            Toast.makeText(CreateAccountActivity.this, "Please enter a valid email.", Toast.LENGTH_LONG).show();
+            return false;
         }
     }
 }
